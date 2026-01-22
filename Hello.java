415b7e8071680ad0c0e9d28cc0b775ec1ac9f509
Fedaa Elmahdi <b00091437@aus.edu>
2026-01-22 18:15:47 +0400

    Feature B added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        featureB();
+
     }
+}
+public static void featureB() {
+    System.out.println("This featureB!");
 }
